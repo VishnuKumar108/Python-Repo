8cd7ae85453cf970614fa0c8ed08fc4d892d372e
Vishnu Kumar Raut <23a91a4462@aec.edu.in>
2025-01-05 13:12:20 +0530

    Create Closest Palindrome.py

---
+++ Closest Palindrome.py
@@ -0,0 +1,31 @@
+Closest Palindrome
+Program Description
+Given a number N, our task is to find the closest Palindrome number whose absolute difference with given number is minimum and absolute difference must be greater than 0.
+
+Input Format
+A single line contains an integer N. 
+
+
+Output Format
+Print the Closest Palindrome Number to the given integer N in a single line. 
+
+Constraints
+0<=N<=100000
+
+Explaination
+Testcase 2 : Both having equal absolute difference with the given number. 
+
+Input-1
+1234
+
+
+Output-1
+1221
+
+
+Input-2
+121
+
+
+Output-2
+131 111
