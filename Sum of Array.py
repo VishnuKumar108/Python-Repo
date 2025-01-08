ddaaa190e528e58fa4fca9804a78186f3ddac862
Vishnu Kumar Raut <23a91a4462@aec.edu.in>
2025-01-08 18:52:57 +0530

    Create Sum of Array.py

---
+++ Sum of Array.py
@@ -0,0 +1,9 @@
+ Input
+n = int(input())  # Read the size of the array
+array = list(map(int, input().split()))  # Read the array elements
+
+# Calculate the sum of the array elements
+array_sum = sum(array)
+
+# Output the result
+print(array_sum)
